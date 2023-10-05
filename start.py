c4a68654d4bffb9e105a31aabbaa881f1db9a3c1
Mira Shlimenzon <tigragoldina@gmail.com>
2023-10-05 16:39:02 -0700

    not sure

--- start.py
+++ start.py
@@ -1,44 +1,49 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-
+from Crypto.Util.Padding import pad
 
 
 def start():
-    # Take in file 
-    file = "mustang.bmp"
+    # Read in file as plain text 
+    with open("mustang.bmp", 'rb') as file:
+        data = file.read()
 
-    # Convert into plain text 
+    # Remove header and get header info
+    # grab header to get meta data and to ensure no encryption of header
+    header = "    "
+    # @ spru need help 
 
-    # Generating random key 
-    key = get_random_bytes(128) 
-    cipher = AES.new(key, AES.MODE_CBC)
+    ecb(data, header) 
+    # or cbc(data, header)
 
-    # Remove header 
+# Function 1: Electronic CodeBook (ECB) 
+# Each block of 128 bytes encrypted independently
+def ecb(data, header):
 
-    # Call function 
+    # thinking this is not the way he wanted us to do this??
+    key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits 
+    cipher = AES.new(key, AES.MODE_ECB) # Instance of AES cipher algorithm for CBC 
+    encrypted_text = cipher.encrypt(pad(data, AES.block_size)) # PKCS#7 padding data
 
-# general function area - take in file, make key
-# before sending file + remove header 
-# call function ECB or CBC 
-# reapply header 
-# output result 
+    # re-append the plaintext BMP headers
+    encrypted_text = header + encrypted_text # so not the way to do this but same idea
 
-# add padding as we loop through 
+    with open("output_ecb", 'wb') as output:
+        output.write(encrypted_text) # rewrite encryption in a new file 
 
+# Function 2: Ciphertext Block Chaining (CBC)
+# Each plaintext block gets XOR-ed with previous ciphertext block prior to encryption
+def cbc(data, header):
+    key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits
+    iv = get_random_bytes(16) # Generating random IV of 16 bytes 
+    cipher = AES.new(key, AES.MODE_CBC, iv) # Instance of AES cipher algorithm for CBC 
+    encrypted_text = cipher.encrypt(pad(data, AES.block_size)) # Padding data
 
+    # re-append the plaintext BMP headers
+    encrypted_text = header + encrypted_text # so not the way to do this but same idea
 
-
-# traverse threw the plaintext using AES following 128 bit increments until the end 
-# grab header to get meta data and to ensure no encryption of header
-# re-append the plaintext BMP headers!)
-# add padding 
-
-# Function 1: ECB 
-
-# Function 2: CBC 
-# generate random IV (case of CBC )
-
-# write encryption of plaintext into new file 
+    with open("output_ecb", 'wb') as output:
+        output.write(encrypted_text) # rewrite encryption in a new file 
 
 if __name__ == "__start__":
     start()
