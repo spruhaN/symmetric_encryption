726b1dd931add0a7e126d1e744979ffd080b7892
Mira Shlimenzon <tigragoldina@gmail.com>
2023-10-21 11:37:53 -0700

    updated let me know what you think

--- start.py
+++ start.py
@@ -7,71 +7,56 @@ def start():
     # Read in file as plain text 
 
     with open('mustang.bmp', 'rb') as file:
-    # Read the first 6 bytes (52 bits) from the file as the header
-    header = file.read(header_length_bits // 8)
-    # Read the rest of the file into another variable
-    data = file.read()
+        header = file.read(54) # Read the first 54-bytes from the file as the header
+         # Read the rest of the file into another variable
+        data = file.read()
 
-    ecb(data, header) 
-    # or cbc(data, header)
+        ecb(data, header) 
+        cbc(data, header)
 
 # Function 1: Electronic CodeBook (ECB) 
 # Each block of 128 bytes encrypted independently
 def ecb(data, header):
-
     # thinking this is not the way he wanted us to do this??
-    key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits 
-    ciphertext = []
-    for 0 in range(len(dataLen))
+    key = get_random_bytes(16)              # Generating random key of 16 bytes or 128 bits 
+    cipher = AES.new(key, AES.MODE_ECB)     # Instance of AES cipher algorithm for CBC 
+    ciphertext = []                         # This will be what we output 
+    ciphertext += header                    # First thing add header to output 
 
-    # only give it one block at a time okay c
-    cipher = AES.new(key, AES.MODE_ECB) # Instance of AES cipher algorithm for CBC 
-    encrypted_text = cipher.encrypt(pad(data, AES.block_size)) # PKCS#7 padding data
-
-    # re-append the plaintext BMP headers
-    encrypted_text = header + encrypted_text # so not the way to do this but same idea
-
+    # only give it one block at a time 
+    for i in range(0, len(data), AES.block_size):
+        block = data[i: i + AES.block_size]
+        padded = pad(block, AES.block_size)    
+    
+        ciphertext += cipher.encrypt(padded)    
+  
     with open("output_ecb.bmp", 'wb') as output:
-        output.write(encrypted_text) # rewrite encryption in a new file 
-
-
-# are they looking for something more like this?
-# def encrypt_ecb(plaintext, key):
-#     ciphertext = []
-#     for block in plaintext_blocks:
-#         encrypted_block = aes_128_encrypt(block, key)
-#         ciphertext.append(encrypted_block)
-#     return ciphertext
-
-# def encrypt_cbc(plaintext, key, iv):
-#     ciphertext = []
-#     previous_block = iv
-#     for block in plaintext_blocks:
-#         # XOR current plaintext block with previous ciphertext block (or IV)
-#         xor_result = xor(block, previous_block)
-#         # Encrypt the XOR result using AES-128
-#         encrypted_block = aes_128_encrypt(xor_result, key)
-#         ciphertext.append(encrypted_block)
-#         # Set the previous ciphertext block to the current encrypted block
-#         previous_block = encrypted_block
-#     return ciphertext
-
+        output.write(bytes(ciphertext)) # rewrite encryption in a new file 
 
 # Function 2: Ciphertext Block Chaining (CBC)
 # Each plaintext block gets XOR-ed with previous ciphertext block prior to encryption
 def cbc(data, header):
+
     key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits
     iv = get_random_bytes(16) # Generating random IV of 16 bytes 
     cipher = AES.new(key, AES.MODE_CBC, iv) # Instance of AES cipher algorithm for CBC 
-    encrypted_text = cipher.encrypt(pad(data, AES.block_size)) # Padding data
+    
+    ciphertext = []                         # This will be what we output 
+    ciphertext += header                    # First thing add header to output 
 
-    # re-append the plaintext BMP headers
-    encrypted_text = header + encrypted_text # so not the way to do this but same idea
+    previous_block = iv
+    for i in range(0, len(data), AES.block_size):
+        block = data[i: i + AES.block_size]
+        padded = pad(block, AES.block_size) 
 
-    with open("output_ecb.bmp", 'wb') as output:
-        output.write(encrypted_text) # rewrite encryption in a new file 
+        xor_block = bytes(bit1 ^ bit2 for bit1, bit2 in zip(padded, previous_block))
 
+        new_block = cipher.encrypt(xor_block)
+        previous_block = new_block
+        ciphertext += new_block
 
+    with open("output_cbc.bmp", 'wb') as output:
+        output.write(bytes(ciphertext)) # rewrite encryption in a new file 
 
-if __name__ == "__start__":
+if __name__ == "__main__":
     start()
