ef8b90cf565a1df299ca899d535a8cc87d3112f9
Mira Shlimenzon <tigragoldina@gmail.com>
2023-10-23 13:55:41 -0700

    still needs work but im making progress

--- task1.py
+++ task1.py
@@ -5,7 +5,6 @@ from Crypto.Util.Padding import pad
 
 def start():
     # Read in file as plain text 
-
     with open('mustang.bmp', 'rb') as file:
         header = file.read(54) # Read the first 54-bytes from the file as the header
          # Read the rest of the file into another variable
@@ -14,11 +13,9 @@ def start():
         ecb(data, header) 
         cbc(data, header)
 
-
-# SPRUUUUU does the cbc and ecb output look correct I'm not so sure? 
-
 # Function 1: Electronic CodeBook (ECB) 
 # Each block of 128 bytes encrypted independently
+# every picture with high contrast one can expect to see results
 def ecb(data, header):
     # thinking this is not the way he wanted us to do this??
     key = get_random_bytes(16)              # Generating random key of 16 bytes or 128 bits 
@@ -29,9 +26,11 @@ def ecb(data, header):
     # only give it one block at a time 
     for i in range(0, len(data), AES.block_size):
         block = data[i: i + AES.block_size]
-        padded = pad(block, AES.block_size)    
-    
-        ciphertext += cipher.encrypt(padded)    
+
+        if(i + AES.block_size > len(data)):
+            block = pad(block, AES.block_size)
+        
+        ciphertext += cipher.encrypt(block) 
   
     with open("output_ecb.bmp", 'wb') as output:
         output.write(bytes(ciphertext)) # rewrite encryption in a new file 
@@ -42,17 +41,19 @@ def cbc(data, header):
 
     key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits
     iv = get_random_bytes(16) # Generating random IV of 16 bytes 
-    cipher = AES.new(key, AES.MODE_CBC, iv) # Instance of AES cipher algorithm for CBC 
+    cipher = AES.new(key, AES.MODE_ECB) # Instance of AES cipher algorithm for ECB
     
     ciphertext = []                         # This will be what we output 
     ciphertext += header                    # First thing add header to output 
-
     previous_block = iv
+
     for i in range(0, len(data), AES.block_size):
         block = data[i: i + AES.block_size]
-        padded = pad(block, AES.block_size) 
+        
+        if(i + AES.block_size > len(data)):
+            block = pad(block, AES.block_size) 
 
-        xor_block = bytes(bit1 ^ bit2 for bit1, bit2 in zip(padded, previous_block))
+        xor_block = bytes(bit1 ^ bit2 for bit1, bit2 in zip(block, previous_block))
 
         new_block = cipher.encrypt(xor_block)
         previous_block = new_block
--- task2.py
+++ task2.py
@@ -6,8 +6,6 @@ from Crypto.Util.Padding import pad
 key = get_random_bytes(16)
 iv = get_random_bytes(16)
 
-
-# THIS NEEDS WORK I started but need help finishing - I had to step back and go study
 def submit(input_string):
     # URL encode any ‘;’ and ‘=’ characters that appear in the user provided string
     newStr = ""
@@ -19,45 +17,58 @@ def submit(input_string):
         else:
             newStr += char
 
+    insert_str = "aaaaaaaaaaaa" # 12 a's - to close block 1 and have it be even block
+    
     # prepending and appending to main string
-    finalStr = "userid=456;userdata=" + newStr + ";session-id=31337"
+    finalStr = "userid=456;userdata=" + insert_str + newStr + ";session-id=31337"
+    ciphertext = cbc(finalStr.encode('utf-8')) 
 
-    # pad the final string (using PKCS#7)
-    paddedStr = pad(finalStr, AES.block_size)
+    return ciphertext
 
-    # encrypt the padded string using AES-128-CBC
-    # @ SPRU this all you! 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = [] 
+def cbc(data):
+    key = get_random_bytes(16) # Generating random key of 16 bytes or 128 bits
+    iv = get_random_bytes(16) # Generating random IV of 16 bytes 
+    cipher = AES.new(key, AES.MODE_ECB) # Instance of AES cipher algorithm for ECB
+    
+    ciphertext = []                    # First thing add header to output 
     previous_block = iv
 
-    for i in range(0, len(paddedStr), AES.block_size):
-        block = paddedStr[i: i + AES.block_size]
-        padded = pad(block, AES.block_size) 
+    for i in range(0, len(data), AES.block_size):
+        block = data[i: i + AES.block_size]
+        
+        if(i + AES.block_size > len(data)):
+            block = pad(block, AES.block_size) 
 
-        xor_block = bytes(bit1 ^ bit2 for bit1, bit2 in zip(padded, previous_block))
+        xor_block = bytes(bit1 ^ bit2 for bit1, bit2 in zip(block, previous_block))
 
         new_block = cipher.encrypt(xor_block)
         previous_block = new_block
         ciphertext += new_block
 
-    print(ciphertext)
     return ciphertext
 
 def update(ciphertext):
-    # need to find a way to modify the ciphertext in such a way that, after decryption?/
-    # How do i do this?? 
-    # need help
-    ciphertext[16] ^= 1 
-    print(ciphertext)
+    # 100 a's / 12 a's 
+
+    # 20 not divisible, add garbage before it 32 , add a repeated character between 33 - 48 (one whole block)
+    # need to change block 2 to make predictable change in block 3
+    # - repeated character block - xor it with something to create predictable 
+
+
+    # C2 = [D] XOR [C1] message2 
+    # C2 XOR with some delta = result will be garbage when xor decrypted message with C1
+    # C3 decrypted XOR with C2 XOR delta = m3 XOR delta = delta to make changes we want in M3 
+    # put repeated same character - A put it in 16 times now we can make figure it out 
+    # A ;  ascii - 
+
+    # M3 is AAAAAA and then you need to figure out what M3 is xored with delta = ;Admin=TRUE; 
+    # Delta injected in block2 and then changes will be seen in block3 
     return ciphertext
 
 def verify(decryptStr):
-    # decrypt string 
-    # @ Spru put your decrypt stuff here
-    cipher = AES.new(key, AES.MODE_ECB, iv)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    print(decryptStr)
     paddedStr = cipher.decrypt(decryptStr)
-    # decrypt over 
 
     # Parse string for patterns 
     return ";admin=true;" in paddedStr
@@ -66,9 +77,10 @@ def verify(decryptStr):
 if __name__ == "__main__":
     input = "You’re the man now, dog"
     cipher = submit(input)
-    result = verify(input)
-    print(result)   # should be false
 
-    modified_ciper = update(cipher)
-    result = verify(input)
-    print(result)   # should be true
+    result = verify(cipher)
+    # print(result)   # should be false
+
+    # modified_ciper = update(cipher)
+    # result = verify(input)
+    # print(result)   # should be true
